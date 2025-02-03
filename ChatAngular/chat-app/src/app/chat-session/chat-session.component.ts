c1a6122b50c9a8b2a1b358ead47b3369d00b78f3
Tóth Benedek <tothbenedek89@gmail.com>
2025-02-03 23:14:09 +0100

    add chat session component

---
+++ ChatAngular/chat-app/src/app/chat-session/chat-session.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-session',
+  imports: [],
+  templateUrl: './chat-session.component.html',
+  styleUrl: './chat-session.component.scss'
+})
+export class ChatSessionComponent {
+
+}
